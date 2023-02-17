a2b496c4119305e87af5e27e8f9463e571194ae9
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-02-16 23:20:01 -0800

    Put together a dummy server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello, World");
+//Imports
+import express, { Request, Response, Application } from "express";
+const dotenv = require("dotenv");
+
+//Set stuff up
+dotenv.config();
+const app: Application = express();
+
+// The port the express app will listen on
+const port = process.env.PORT || 3000;
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
+
+// Dummy route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
