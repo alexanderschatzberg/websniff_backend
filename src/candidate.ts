d4d5979ebf23730054ecf456fba4f9b3205e5600
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-02-17 23:35:18 -0800

    Comments

--- src/candidate.ts
+++ src/candidate.ts
@@ -13,13 +13,14 @@ const prisma = new PrismaClient();
 
 //Upload a candidate
 candidateRouter.post("/", async (req: Request, res: Response) => {
-  const { name, ra, dec } = req.body;
+  const { name, ra, dec } = req.body; //ra and dec are in degrees
   if (!name || !ra || !dec) {
+    //Check if all fields are present
     res.status(400).json({ message: "Missing name, ra, or dec" });
     return;
   }
 
-  await prisma.candidate
+  await prisma.candidate //Create the candidate
     .create({
       data: {
         id: v4(),
@@ -29,10 +30,11 @@ candidateRouter.post("/", async (req: Request, res: Response) => {
       },
     })
     .then((candidate: Candidate) => {
+      //If successful, return the candidate
       return res
         .status(200)
         .json({ message: "Candidate successfully created", candidate });
-    })
+    }) //If not, check if it's a duplicate and return an error
     .catch((err: any) => {
       if (err.code === "P2002") {
         res.status(400).json({
@@ -48,14 +50,15 @@ candidateRouter.post("/", async (req: Request, res: Response) => {
 
 //Get a candidate by id
 candidateRouter.get("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  await prisma.candidate
+  const { id } = req.params; //Get the id from the url
+  await prisma.candidate //Find the candidate
     .findUnique({
       where: {
         id,
       },
     })
     .then((candidate: Candidate | null) => {
+      //If successful, return the candidate
       if (!candidate)
         return res.status(404).json({ message: "Candidate not found" });
 
@@ -63,6 +66,7 @@ candidateRouter.get("/:id", async (req: Request, res: Response) => {
       return;
     })
     .catch((err: any) => {
+      //If not, return an error
       res.status(500).json({ message: "Error finding candidate", error: err });
       return;
     });
@@ -70,7 +74,7 @@ candidateRouter.get("/:id", async (req: Request, res: Response) => {
 
 //Get all candidates
 candidateRouter.get("/", async (req: Request, res: Response) => {
-  const candidates: void | Candidate[] | null = await prisma.candidate
+  const candidates: void | Candidate[] | null = await prisma.candidate //Find all candidates
     .findMany()
     .catch((err: any) => {
       res.status(500).json({ message: "Error finding candidates", error: err });
@@ -78,9 +82,10 @@ candidateRouter.get("/", async (req: Request, res: Response) => {
     });
 
   if (!candidates)
+    //If there are no candidates, return an error
     return res.status(404).json({ message: "No candidates found" });
 
-  return res.status(200).json({ message: "Candidates Found", candidates });
+  return res.status(200).json({ message: "Candidates Found", candidates }); //If successful, return the candidates
 });
 
 //Exports
